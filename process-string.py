53c14998fdd7b910447334d8f3a6806f2db1d5e6
Sreyno <156500813+sreyno7@users.noreply.github.com>
2024-01-28 15:11:39 +0700

    Update process-string.py

--- process-string.py
+++ process-string.py
@@ -6,6 +6,11 @@
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
 # * "1234!_ "     -> "11223344!!__  "
 
+str = input("Enter a string: ")
+result = ''
+for letters in str:
+    result += letters*2
+print (result)
 
 # Application 2
 #  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
@@ -21,4 +26,16 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 user_range = input("Enter a range of letters (e.g., a-z): ")
 
+def get_letter_range(range_str):
+    start, end = range_str[0], range_str[2]
 
+    start_val = ord(start)
+    end_val = ord(end)
+    
+    # Generate the range of characters
+    list_of_letter = []
+    for i in range(start_val, end_val + 1):
+        list_of_letter.append(chr(i))
+    final_answer = "".join(list_of_letter)
+    
+    return final_answer
